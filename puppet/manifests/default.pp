768e989fb75f21851f0b9abbe48e90684a1ad78c
Adam Evans <adam@agileand.me>
2012-08-05 03:21:45 +0100

    Added mongodb and exposed the port in vagrant.

---
+++ puppet/manifests/default.pp
@@ -0,0 +1,27 @@
+stage { pre: before => Stage[main] }
+
+class apt_get_update {
+  $sentinel = "/var/lib/apt/first-puppet-run"
+
+  exec { "initial apt-get update":
+    command => "/usr/bin/apt-get update && touch ${sentinel}",
+    onlyif  => "/usr/bin/env test \\! -f ${sentinel} || /usr/bin/env test \\! -z \"$(find /etc/apt -type f -cnewer ${sentinel})\"",
+    timeout => 3600,
+  }
+}
+
+# Run apt-get update prior to testing
+class { 'apt_get_update':
+  stage => pre,
+}
+
+package { 'mongodb':
+  ensure => present,
+}
+
+service { 'mongodb':
+  ensure  => running,
+  require => Package['mongodb'],
+}
+
+# sudo sed -i 's/bind_ip = 127.0.0.1/bind_ip = 0.0.0.0/g' /etc/mongodb.conf
